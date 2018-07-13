b55bbe6e91b1a486abee3b0a457231c806588d74
May <nontaya@drivehub.co>
2018-07-13 12:54:45 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Button, Jumbotron } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Jumbotron>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <h2 className="App-title">Hello World !!</h2>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Button bsStyle="warning" bsSize="large"> Test Bootstrap Button </Button>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
