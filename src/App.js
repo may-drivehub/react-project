6043590adee6386566b83649de231e794bc12bdc
May <nontaya@drivehub.co>
2018-07-16 21:12:27 +0700

    test dev

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
           </header>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
+            This is dev branch.
           </p>
           <Button bsStyle="warning" bsSize="large"> Test Bootstrap Button </Button>
         </Jumbotron>
